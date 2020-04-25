c9ac5199fd77b70f0cb7ad0d3cd2497d06caa1ac
Hanhkhph08082 <kieuhonghanh2000@gmail.com>
2020-04-25 22:20:04 +0700

    1 file modified

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable()
 export class ProductService {
-api = 'https://5e7c2234a917d70016682ea2.mockapi.io/products';
+api = 'https://5ea453ba270de600164603c3.mockapi.io/product';
  products: Product;
   constructor(private http: HttpClient) { }
 getListProduct(): Observable<Product[]> {
